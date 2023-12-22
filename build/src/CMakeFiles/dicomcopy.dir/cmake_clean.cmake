923197d7b9835071b0761ad3b241385c9f39e2fa
Funnypinky <steffenhasler@googlemail.com>
2023-12-22 04:54:59 +0100

    Init Repo

---
+++ build/src/CMakeFiles/dicomcopy.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dicomcopy.dir/main.cpp.o"
+  "CMakeFiles/dicomcopy.dir/main.cpp.o.d"
+  "dicomcopy"
+  "dicomcopy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dicomcopy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "gdcmReader.h"
+#include "gdcmWriter.h"
+#include "gdcmAttribute.h"
+#include "QApplication"
+
+int main() {
+    std::cout << "Hello World" << std::endl;
+    gdcm::Reader reader;
+    return 0;
+}
